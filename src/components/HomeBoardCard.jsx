f8722fbb70e2bb291144d339911b4a6d3581f06f
yogesh19aggarwal <yogesh.19.aggarwal@gmail.com>
2025-01-22 20:25:36 +0530

    Add board

--- src/components/HomeBoardCard.jsx
+++ src/components/HomeBoardCard.jsx
@@ -1,7 +1,12 @@
+import { Box } from "@mui/material"
 
-const HomeBoardCard = () => {
+const HomeBoardCard = ({board}) => {
+    // console.log(board.prefs.backgroundImage);
+    
   return (
-    <div>HomeBoardCard</div>
+    <Box style={{background: `url(${board.prefs.backgroundImage})`, width:'300px', height:'200px', backgroundSize:'cover', backgroundPosition:'center', padding:'0.5rem', color:'white', cursor:'pointer', borderRadius:'10px', boxShadow:'0 2px 5px 0px rgba(0,0,0)'}}>
+        <h2>{board.name}</h2>
+    </Box>
   )
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react"
-import { Box } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Box, Grid2 } from "@mui/material";
 
 import { fetchBoards } from "../api/fetchApi";
 import HomeBoardCard from "../components/HomeBoardCard";
@@ -8,21 +8,34 @@ const HomePage = () => {
 
   const [boards, setBoards] = useState([]);
 
-  useEffect(()=>{
-    fetchBoards().then((res)=>{
-      setBoards(res)
-      
-    }).catch((err)=>{
-      console.log(err);
-    })
-    
-  },[]);
+  useEffect(() => {
+    fetchBoards()
+      .then((res) => {
+        console.log(res);
+        
+        setBoards(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
-    <>
-      <Box
-    </>
-  )
-}
+    <Box sx={{ flexGrow: 1, p: 2, margin:'3rem 1rem' }}>
+      <Grid2 
+        container 
+        xs={12} // Full width on extra-small screens
+        sm={6}  // Half width on small screens
+        md={4}  // One-third width on medium screens
+        lg={3}  // One-fourth width on large screens
+        spacing={4} // Adds spacing between Grid2 items
+      >
+        {boards.map((board, index) => (
+            <HomeBoardCard key={index} board={board} />
+        ))}
+      </Grid2>
+    </Box>
+  );
+};
 
-export default HomePage
+export default HomePage;
