cbb8ba8b11cb087e0849c827c17adb3ba39004e7
yogesh19aggarwal <yogesh.19.aggarwal@gmail.com>
2025-01-24 17:33:17 +0530

    update url in useNavgiate

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -33,7 +33,7 @@ const HomePage = () => {
       setBoards([...boards, response.data]);
       setBoardName("");
       setOpenPopover(null);
-      navigate(`boards/${response.data.id}`);
+      navigate(`/boards/${response.data.id}`);
     } catch (error) {
       throw new Error(`${error}`)
     }
