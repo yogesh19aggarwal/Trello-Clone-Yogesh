477b1db49063b8bf13180e862bde021130b5f2ec
yogesh19aggarwal <yogesh.19.aggarwal@gmail.com>
2025-02-07 12:01:29 +0530

    Implement Signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import BoardPage from "./pages/BoardPage";
 import NotFound from "./pages/NotFound";
 import MainLayout from "./layouts/MainLayout";
 import ErrorBoundary from "./errorBoundary/ErrorBoundary";
+import SignIn from "./components/auth/SignIn";
 
 const App = () => {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
     <ErrorBoundary>
       <Routes>
-        <Route path="/" element={<Navigate to="/boards" replace />} />
+        <Route path="/" element={isAuthenticated ? <Navigate to="/boards" replace /> : <Navigate to="/signin" replace />} />
 
-        <Route path="/" element={<MainLayout />}>
-          <Route path="/boards" element={<ErrorBoundary><HomePage/></ErrorBoundary>} />
-          <Route path="boards/:boardId" element={<ErrorBoundary><BoardPage/></ErrorBoundary>} />
-          <Route path="*" element={<ErrorBoundary><NotFound/></ErrorBoundary>} />
-        </Route>
+        <Route path="/signin" element={<SignIn setIsAuthenticated={setIsAuthenticated} />} />
 
+        {isAuthenticated ? (
+          <Route path="/" element={<MainLayout />}>
+            <Route path="/boards" element={<ErrorBoundary><HomePage /></ErrorBoundary>} />
+            <Route path="boards/:boardId" element={<ErrorBoundary><BoardPage /></ErrorBoundary>} />
+          </Route>
+        ) : (
+          <Route path="*" element={<Navigate to="/signin" replace />} />
+        )}
+
+        <Route path="*" element={<ErrorBoundary><NotFound /></ErrorBoundary>} />
       </Routes>
     </ErrorBoundary>
   );
